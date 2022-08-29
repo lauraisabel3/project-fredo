782c8df677516197a94ac145ec2c7d727af342b6
lauraisabel3 <lauraisabel543@gmail.com>
2022-08-28 22:20:19 -0500

    Add stories

---
+++ src/stories/components/Cards.stories.jsx
@@ -0,0 +1,10 @@
+import Cards from "../../components/Cards";
+import { Container, Box,Image, Tag, Heading,Text, Icon } from "@chakra-ui/react"
+
+
+export default {
+  title: 'Components/Cards',
+  component: Cards,
+};
+
+export const CardsPage = () => <Cards/>
---
+++ stories/pages/cards.stories.js
@@ -0,0 +1,8 @@
+import Cards from "../../src/components/Cards";
+
+export default {
+  title: 'pages/Cards',
+  component: Cards,
+}
+
+export const Card = () => <Cards/>
